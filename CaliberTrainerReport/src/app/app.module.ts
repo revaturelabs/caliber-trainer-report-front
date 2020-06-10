ece16e388cc1aac3bb866b7ec0910757beca9ea9
Raul Gonzalez <rgonz166@gmail.com>
2020-06-10 11:50:01 -0700

    Add: qc home and assessment component to app module

--- CaliberTrainerReport/src/app/app.module.ts
+++ CaliberTrainerReport/src/app/app.module.ts
@@ -7,14 +7,14 @@ import { AppComponent } from './app.component';
 import { TestChartComponent } from './Components/test-chart/test-chart.component';
 import { FirstChartService } from './first-chart.service';
 import { MainNavBarComponent } from './Components/main-nav-bar/main-nav-bar.component';
-import { QCBatchesTechnicalStatusComponent } from './qcbatches-technical-status/qcbatches-technical-status.component';
-import { QCBatchesIndivCategoryTechnicalStatusComponent } from './qcbatches-indiv-category-technical-status/qcbatches-indiv-category-technical-status.component';
-import { QCBatchesWeekCategoryTechnicalStatusComponent } from './qcbatches-week-category-technical-status/qcbatches-week-category-technical-status.component';
-import { AssessmentBatchesTechnicalStatusComponent } from './assessment-batches-technical-status/assessment-batches-technical-status.component';
-import { AssessmentBatchesIndivCategoryTechnicalStatusComponent } from './assessment-batches-indiv-category-technical-status/assessment-batches-indiv-category-technical-status.component';
-import { AssessmentBatchesTrendCategoryTechnicalStatusComponent } from './assessment-batches-trend-category-technical-status/assessment-batches-trend-category-technical-status.component';
-import { QCComponent } from './qc/qc.component';
-import { AssessmentComponent } from './assessment/assessment.component';
+import { QCBatchesTechnicalStatusComponent } from './Components/qcbatches-technical-status/qcbatches-technical-status.component';
+import { QCBatchesIndivCategoryTechnicalStatusComponent } from './Components/qcbatches-indiv-category-technical-status/qcbatches-indiv-category-technical-status.component';
+import { QCBatchesWeekCategoryTechnicalStatusComponent } from './Components/qcbatches-week-category-technical-status/qcbatches-week-category-technical-status.component';
+import { AssessmentBatchesTechnicalStatusComponent } from './Components/assessment-batches-technical-status/assessment-batches-technical-status.component';
+import { AssessmentBatchesIndivCategoryTechnicalStatusComponent } from './Components/assessment-batches-indiv-category-technical-status/assessment-batches-indiv-category-technical-status.component';
+import { AssessmentBatchesTrendCategoryTechnicalStatusComponent } from './Components/assessment-batches-trend-category-technical-status/assessment-batches-trend-category-technical-status.component';
+import { QCComponent } from './Components/qc/qc.component';
+import { AssessmentComponent } from './Components/assessment/assessment.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
