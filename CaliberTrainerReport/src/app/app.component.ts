0ec112e8fd1c3eea8114bf093a053fd962436e36
Jordy Then <42790442+jordythen@users.noreply.github.com>
2020-06-15 14:48:35 -0400

    fixed up loading graph

--- CaliberTrainerReport/src/app/app.component.ts
+++ CaliberTrainerReport/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { UrlService } from 'src/app/url.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  dataIsDoneLoading: any;
+  constructor(private urlService: UrlService) { }
+
+  ngOnInit(): void {
+    this.urlService.initializeData().subscribe(
+      resp => {
+        this.dataIsDoneLoading = resp.status;
+        console.log("RESP: "+resp + "And status code: " + this.dataIsDoneLoading);
+
+        if(this.dataIsDoneLoading === 200){
+          console.log("Data is done loading.");
+        }
+      }
+    );
+  }
   title = 'CaliberTrainerReport';
 }
