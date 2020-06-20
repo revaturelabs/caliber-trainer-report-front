e138edfc459cc2c4e0d994c6910794f206094b6d
Raul Gonzalez <rgonz166@gmail.com>
2020-06-19 23:29:52 -0700

    Fix: sonar cloud code smells extra semicolon and unused import

--- CaliberTrainerReport/src/app/Components/progresspage/progress.component.ts
+++ CaliberTrainerReport/src/app/Components/progresspage/progress.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FileUploadService } from '../../file-upload.service';
 import { Observable } from 'rxjs';
 
