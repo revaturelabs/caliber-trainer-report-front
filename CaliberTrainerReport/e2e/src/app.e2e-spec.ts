de86eb687a698819b77d0f8f12eff861c9ff6a00
Jordy Then <42790442+jordythen@users.noreply.github.com>
2020-06-20 21:05:31 -0400

    added some more protractor test for graph existence and file upload stuff

--- CaliberTrainerReport/e2e/src/app.e2e-spec.ts
+++ CaliberTrainerReport/e2e/src/app.e2e-spec.ts
@@ -2,33 +2,38 @@ import { AppPage } from './app.po';
 import { browser, by, element } from 'protractor';
 
 
-describe('Testing Navigation and Top Navbar', () => {
+xdescribe('Testing Navigation and Top Navbar', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
+  // 1
   it('should display the title of the application on the navbar on wide screen', () => {
     page.navigateTo();
     expect(page.getNavBarTitle(1920, 1080)).toEqual('EXCALIBER');
   });
 
+  // 2
   it('should display Revature icon on navbar', () => {
     page.navigateTo();
     expect(page.getNavBarRevatureIcon().isPresent()).toBe(true);
   });
 
+  // 3
   it('should display QC button on navbar', () => {
     page.navigateTo();
     expect(page.getNavBarViewQCReportsButton(1920, 1080).getText()).toBe('View QC Reports');
   });
 
+  // 4
   it('should display Assessment button on navbar', () => {
     page.navigateTo();
     expect(page.getNavBarViewAssessmentReportsButton(1920, 1080).getText()).toBe('View Assessment Reports');
   });
 
+  // 5
   it('should display QC button under Hamburger collapse button', () => {
     page.navigateTo();
     browser.driver.manage().window().setSize(315, 812);
@@ -36,6 +41,7 @@ describe('Testing Navigation and Top Navbar', () => {
     expect(element(by.css('[href="/qc"]')).getText()).toBe('View QC Reports');
   });
 
+  // 6
   it('should display Assessment button under Hamburger collapse button', () => {
     page.navigateTo();
     browser.driver.manage().window().setSize(315, 812);
@@ -43,16 +49,106 @@ describe('Testing Navigation and Top Navbar', () => {
     expect(element(by.css('[href="/assessment"]')).getText()).toBe('View Assessment Reports');
   });
 
-  it('should navigate to QC assessment reports page from Homepage', () => {
+  // 7
+  it('should navigate to QC reports page from Homepage', () => {
     page.navigateTo();
     page.getNavBarViewQCReportsButton(1920, 1080).click();
     expect(element(by.css('app-qc h2')).getText()).toBe('QC Technical Status');
   });
 
-  it('should navigate to QC assessment reports page from Homepage', () => {
+  // 8
+  it('should navigate to assessment reports page from Homepage', () => {
     page.navigateTo();
     page.getNavBarViewAssessmentReportsButton(1920, 1080).click();
     expect(element(by.css('app-assessment h2')).getText()).toBe('Assessment Technical Status');
   });
 
+  // 9
+  it('should navigate to QC reports page with Hamburger Icon', () => {
+    page.navigateTo();
+    browser.driver.manage().window().setSize(315, 812);
+    element(by.css('app-main-nav-bar .hambIcon')).click();
+    element(by.id('demo2')).click();
+    expect(element(by.css('app-qc h2')).getText()).toBe('QC Technical Status');
+  });
+
+  // 10
+  it('should navigate to Assessment reports page with Hamburger Icon', () => {
+    page.navigateTo();
+    browser.driver.manage().window().setSize(315, 812);
+    element(by.css('app-main-nav-bar .hambIcon')).click();
+    element(by.id('demo3')).click();
+    expect(element(by.css('app-assessment h2')).getText()).toBe('Assessment Technical Status');
+  });
+
 });
+
+fdescribe('(NEGATIVE)Testing graph display', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  // 15
+  fit('should not display qc batches technical status chart without uploading json file', () => {
+    page.navigateTo();
+    page.getNavBarViewQCReportsButton(1920, 1080).click();
+    expect(element(by.css('app-qcbatches-technical-status #divChart1 .chartjs-render-monitor')).isPresent()).toBe(false);
+  });
+
+  // 16
+  fit('should not display qc batches individual category technical chart without uploading json file', () => {
+    page.navigateTo();
+    page.getNavBarViewQCReportsButton(1920, 1080).click();
+    expect(element(by.css('app-qcbatches-indiv-category-technical-status #divChart2 .chartjs-render-monitor')).isPresent()).toBe(false);
+  });
+
+  // 17
+  fit('should not display qc week category technical chart without uploading json file', () => {
+    page.navigateTo();
+    page.getNavBarViewQCReportsButton(1920, 1080).click();
+    expect(element(by.css('app-qcbatches-week-category-technical-status #divChart3 .chartjs-render-monitor')).isPresent()).toBe(false);
+  });
+});
+
+describe('(POSITIVE)Testing file upload and graph display', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  // 11
+  it('should upload JSON file', () => {
+    page.navigateTo();
+    page.uploadTestJSONFile();
+    expect(element(by.css('app-progress .uploaded-file')).isPresent()).toBe(true);
+  });
+
+  // 12
+  it('should display qc batches technical status chart after json file upload', () => {
+    page.navigateTo();
+    page.uploadTestJSONFile();
+    page.getNavBarViewQCReportsButton(1920, 1080).click();
+    expect(element(by.css('app-qcbatches-technical-status #divChart1 .chartjs-render-monitor')).isPresent()).toBe(true);
+  });
+
+  // 13
+  it('should display qc batches individual category technical status chart after json file upload', () => {
+    page.navigateTo();
+    page.uploadTestJSONFile();
+    page.getNavBarViewQCReportsButton(1920, 1080).click();
+    expect(element(by.css('app-qcbatches-indiv-category-technical-status #divChart2 .chartjs-render-monitor')).isPresent()).toBe(true);
+  });
+
+  // 14
+  it('should display qc week category technical status chart after json file upload', () => {
+    page.navigateTo();
+    page.uploadTestJSONFile();
+    page.getNavBarViewQCReportsButton(1920, 1080).click();
+    expect(element(by.css('app-qcbatches-week-category-technical-status #divChart3 .chartjs-render-monitor')).isPresent()).toBe(true);
+  });
+
+});
+
--- CaliberTrainerReport/e2e/src/app.po.ts
+++ CaliberTrainerReport/e2e/src/app.po.ts
@@ -10,62 +10,27 @@ export class AppPage {
     return element(by.css('app-main-nav-bar div.mainTitle')).getText() as Promise<any>;
   }
 
-  getNavBarRevatureIcon(){
+  getNavBarRevatureIcon() {
     return element(by.css('[src = "https://imgur.com/QMNMf4M.png"]'));
   }
 
-  getNavBarViewQCReportsButton(width, height){
+  getNavBarViewQCReportsButton(width, height) {
     browser.driver.manage().window().setSize(width, height);
     return element(by.css('[routerLink = "/qc"]'));
   }
 
-  getNavBarViewAssessmentReportsButton(width, height){
+  getNavBarViewAssessmentReportsButton(width, height) {
     browser.driver.manage().window().setSize(width, height);
     return element(by.css('[routerLink = "/assessment"]'));
   }
 
+  uploadTestJSONFile() {
+    const path = require('path');
+    const fileToUpload = '../JSONTestFiles/data.json',
+      absolutePath = path.resolve(__dirname, fileToUpload);
 
-  getAllGraphsOnPage() {
-    return element.all(by.id('divChart'));
-  }
-
-  // First Test Protractor gets the Title Text
-  getTitleText(): Promise<string> {
-    return element(by.tagName('app-test-chart')).getText() as Promise<string>;
-  }
-
-  // Second Test Gets the Canvas Table
-  getCanvasTable(): Promise<any> {
-    return element(by.tagName('canvas')).getText() as Promise<any>;
+    element(by.css('input[type="file"]')).sendKeys(absolutePath);
+    element(by.css('input[type="submit"]')).click();
   }
 
-  // Third Test Changes teh Canvas Table
-  changeCanvasTable(): Promise<any> {
-    return element(by.tagName('label')).getText() as Promise<any>;
-  }
-  selectPrevLabel() {
-    browser
-      .actions()
-      .click()
-
-  }
-  selectNextLabel() {
-    browser
-      .actions()
-      .click()
-  }
-
-  getAllGraphs() {
-    browser.sleep(3000);
-    browser.driver.manage().window().setSize(1920, 1080); // isBig = true
-    element(by.id('demo')).click();
-    return element.all(by.tagName('canvas'));
-  }
-  getAllGraphsMobileView() {
-    browser.sleep(3000);
-    browser.driver.manage().window().setSize(1000, 1080); // isBig = false
-    element(by.className('hambIcon')).click();
-    element(by.id('demo2')).click();
-    return element.all(by.tagName('canvas'));
-  }
 }
