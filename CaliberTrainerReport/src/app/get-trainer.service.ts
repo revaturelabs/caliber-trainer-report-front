e7750ee2647ad3d947260ed6d2b4c5ad0b6465f4
Raul Gonzalez <rgonz166@gmail.com>
2020-06-23 12:45:23 -0700

    Removed: unused map import from get-trainer service

--- CaliberTrainerReport/src/app/get-trainer.service.ts
+++ CaliberTrainerReport/src/app/get-trainer.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Trainer } from './class/trainer'; // trainer class
 import { HttpClient } from '@angular/common/http';
 import {BehaviorSubject, Observable} from 'rxjs';
-import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
