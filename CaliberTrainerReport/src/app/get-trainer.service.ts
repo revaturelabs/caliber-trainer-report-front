686f4e668f07c7b17d40a8fcfcc4d69cde763cd0
Patrick Boronski <plboronski@gmail.com>
2020-09-10 17:35:11 -0400

    Set up mock data for user while CORS issues are resolved

--- CaliberTrainerReport/src/app/get-trainer.service.ts
+++ CaliberTrainerReport/src/app/get-trainer.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Trainer } from './class/trainer'; // trainer class
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {BehaviorSubject, Observable} from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { UrlService } from './url.service';
@@ -13,17 +13,26 @@ export class GetTrainerService {
   currentTrainers: Observable<Trainer>;
   currentTrainerSubject: BehaviorSubject<Trainer>;
 
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Headers':'Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Access-Control-Allow-Headers,Authorization,Accept,Content-Type,Origin,Host,Referer,X-Requested-With,X-CSRF-Token',
+    'Access-Control-Allow-Origin': 'http://localhost:4200',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    'Access-Control-Allow-Credentials': 'true'
+  });
+  
   constructor(private http: HttpClient, private urlServe: UrlService) { }
-
+  
   getTrainer(trainer: Trainer) {
     return trainer;
   }
-
+  
   getTrainerId(trainer: Trainer) {
     return trainer.id;
   }
-
+  
   async getTrainerList() {
+    
     // return this.http.get<Trainer>(`${environment.backEndUrl}Trainer`)
     //   .pipe(map(result => {
     //     // store Trainer details and jwt token in local storage to keep Trainer details in between page refreshes
@@ -41,8 +50,8 @@ export class GetTrainerService {
   }
 
 
-  getAllTrainers(){
-    return this.http.get<Trainer[]>(this.urlServe.getCaliberUrl() + "training/batch/trainers");
+  getAllTrainers() : Observable<Trainer[]>{
+    return this.http.get<Trainer[]>(this.urlServe.getCaliberUrl() + "training/batch/trainers", {headers: this.headers});
   }
 
   // To Reduce complexity, and reuse the function, we could try to figure out how to synchronize this function call properly
