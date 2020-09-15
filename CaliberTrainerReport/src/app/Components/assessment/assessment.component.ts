93f0f7d00ac98f6edcd7bca26d6c9da57f127f6a
mitchgreene92 <mitchgreene92@gmail.com>
2020-09-15 16:40:32 -0500

    adding print-to-pdf functionality

--- CaliberTrainerReport/src/app/Components/assessment/assessment.component.ts
+++ CaliberTrainerReport/src/app/Components/assessment/assessment.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, DoCheck } from '@angular/core';
-
+import html2PDF from 'html2pdf.js';
 @Component({
   selector: 'app-assessment',
   templateUrl: './assessment.component.html',
@@ -22,4 +22,20 @@ export class AssessmentComponent implements OnInit, DoCheck {
     this.selectedValue = graphSelector.value;
   }
 
+  public downloadPDF() {
+    const options = {
+      filename: 'testFile.pdf',
+      image: {type: 'jpeg', quality: 1},
+      html2canvas: {scale: 1},
+      jsPDF: {orientation: 'landscape'}
+    };
+ 
+    const content: Element = document.getElementById('assessment-body');
+ 
+    html2PDF()
+    .from(content)
+    .set(options)
+    .save();
+   }
+ 
 }
