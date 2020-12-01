3d7a90ffec1813bb5a45878e36f5cbc15b8efa89
kr4N7z <73358932+kr4N7z@users.noreply.github.com>
2020-11-30 18:44:40 -0600

    Revert "revert"
    This reverts commit 00e3e3bcb5c7390a6b34639debc9b0c7839a5561.

--- CaliberTrainerReport/src/app/Components/qc-technical-scores-by-category-across-batches/qc-technical-scores-by-category-across-batches.component.ts
+++ CaliberTrainerReport/src/app/Components/qc-technical-scores-by-category-across-batches/qc-technical-scores-by-category-across-batches.component.ts
@@ -5,7 +5,6 @@ import { BatchTechnicalStatusBySkillCategoryService } from 'src/app/services/Bat
 import { QCComponent } from 'src/app/Components/qc/qc.component';
 import { Subscription } from 'rxjs';
 import { DisplayGraphService } from 'src/app/services/display-graph.service';
-import { FilterBatch } from 'src/app/utility/FilterBatch';
 
 
 @Component({
@@ -38,13 +37,6 @@ export class QcTechnicalScoresByCategoryAcrossBatchesComponent
   batchNames: string[];
   batchFlags: string[];
   yValues: any[];
-  
-  // this array tracks which batches to show on the graph
-  // index of batchFlags corresponds to index of batchNames:string[]
-  batchFlags: boolean[];
-  // FilterBatch is a helper class located in utility folder under src > app
-  // it contains a method called filterBatch(any[], boolean[]) that takes in any[] and returns a new any[] with true indices from boolean[]
-  batchFilter: FilterBatch;
 
   // Dealing with Scalability
   width: number;
@@ -74,8 +66,6 @@ export class QcTechnicalScoresByCategoryAcrossBatchesComponent
     this.cumulativeGood = [];
     this.cumulativePoor = [];
 
-    this.batchFlags = [];
-    this.batchFilter = new FilterBatch();
 
     this.BatchTechnicalStatusBySkillCategoryServiceSubscription = this.batchTechnicalStatusBySkillCategoryService
       .getAvgCategoryScoresObservables()
@@ -104,11 +94,10 @@ export class QcTechnicalScoresByCategoryAcrossBatchesComponent
         
         for (const score of resp.batchByCategory[0].batches) {
           this.batchNames.push(score.batchName);
-          this.batchFlags.push(true);
         }
 
         // These arguments might need to change.
-        this.displayGraph(this.batchFilter.filterBatch(this.batchNames,this.batchFlags), this.batchFilter.filterBatch(this.yValues,this.batchFlags));
+        this.displayGraph(this.batchNames, this.yValues);
       });
   }
 
@@ -129,10 +118,9 @@ export class QcTechnicalScoresByCategoryAcrossBatchesComponent
 
     this.setScoreValues();
     if(this.selectedValue ==0 ){
-      //this.displayGraph(gA2[0], gA2[1]);
-      this.displayGraph(this.batchFilter.filterBatch(gA2[0],this.batchFlags), this.batchFilter.filterBatch(gA2[1],this.batchFlags));
+      this.displayGraph(gA2[0], gA2[1]);
     } else {
-      this.displayGraph(this.batchFilter.filterBatch(this.batchNames,this.batchFlags), this.batchFilter.filterBatch(this.yValues,this.batchFlags));
+      this.displayGraph(this.batchNames, this.yValues);
     }
   }
 
@@ -250,7 +238,7 @@ export class QcTechnicalScoresByCategoryAcrossBatchesComponent
 
         let dataObj = {
           label: ''+this.categoriesName[i], // Name the series
-          data: this.batchFilter.filterBatch(this.multiGraphYValues[i-1],this.batchFlags), // Specify the data values array
+          data: this.multiGraphYValues[i-1], // Specify the data values array
           fill: false,
           borderColor: lineColor, // Add custom color border (Line)
           backgroundColor: '#000000', // Add custom color background (Points and Fill)
@@ -396,15 +384,5 @@ export class QcTechnicalScoresByCategoryAcrossBatchesComponent
 
       this.BatchTechnicalStatusBySkillCategoryServiceSubscription.unsubscribe();
     }
-  }
-
-  toggle(index: number): void{
-    this.batchFlags[index] = !this.batchFlags[index];
-    this.updateGraph(); 
-  }
-
-  batch_dropdown_flag: boolean = true;
-  toggleBatchDropdown(): void{
-    this.batch_dropdown_flag = !this.batch_dropdown_flag;
   }
 }
