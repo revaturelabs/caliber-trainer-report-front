00e3e3bcb5c7390a6b34639debc9b0c7839a5561
kr4N7z <73358932+kr4N7z@users.noreply.github.com>
2020-11-30 18:43:18 -0600

    revert

--- CaliberTrainerReport/src/app/Components/review-page-total-avg-assessment/review-page-total-avg-assessment.component.spec.ts
+++ CaliberTrainerReport/src/app/Components/review-page-total-avg-assessment/review-page-total-avg-assessment.component.spec.ts
@@ -1,28 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import {DebugElement} from '@angular/core';
+
 import { ReviewPageTotalAvgAssessmentComponent } from './review-page-total-avg-assessment.component';
-import { HttpClientModule } from '@angular/common/http';
-import {By} from '@angular/platform-browser';
-import { of } from 'rxjs';
-import { AssessmentByBatchService } from 'src/app/services/AssessmentByBatch.service';
 
 describe('ReviewPageTotalAvgAssessmentComponent', () => {
   let component: ReviewPageTotalAvgAssessmentComponent;
-  let batch = {
-    score: {
-      poor: 4,
-      average: 10,
-      good: 7,
-      superstar: 2
-    }
-  }
   let fixture: ComponentFixture<ReviewPageTotalAvgAssessmentComponent>;
-  let de: DebugElement;
+
   beforeEach(async(() => {
-    
     TestBed.configureTestingModule({
-      declarations: [ ReviewPageTotalAvgAssessmentComponent ],
-      imports: [HttpClientModule]
+      declarations: [ ReviewPageTotalAvgAssessmentComponent ]
     })
     .compileComponents();
   }));
@@ -30,27 +16,10 @@ describe('ReviewPageTotalAvgAssessmentComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ReviewPageTotalAvgAssessmentComponent);
     component = fixture.componentInstance;
-    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  
-  it('should have a span tag of `Total Average Assessment Scores`', () =>{
-    expect(de.query(By.css('span')).nativeElement.innerText).toBe('Total Average Assessment Scores');
-  });
-
-  it('chart should be undefined at start', () =>{
-    expect(component.myLineChart).toBeUndefined();
-  });
-
-  it('graph should be displayed', () =>{
-    expect(component.displayGraph).toHaveBeenCalled;
-  })
-
- 
-
-
 });
