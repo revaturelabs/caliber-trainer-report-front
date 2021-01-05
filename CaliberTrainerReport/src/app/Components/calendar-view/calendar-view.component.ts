f379e0b167aceee60a64e799a975470f25bd6274
Lydia  Bierer <lbierer@gmail.com>
2021-01-05 12:02:46 -0600

    calendar implementation

--- CaliberTrainerReport/src/app/Components/calendar-view/calendar-view.component.ts
+++ CaliberTrainerReport/src/app/Components/calendar-view/calendar-view.component.ts
@@ -12,12 +12,21 @@ export class CalendarViewComponent implements OnInit {
 
    batches:any[] =[];
   dataSource: Data[] = [];
+  years = [2015,2016,2017,2018,2019,2020];
+  year=0;
+  yearObj=[];
+  allYears=[];
+  pickedYear:any;
 
   constructor(private batchServ: GetBatchService) { }
 
-
   ngOnInit(){
 
+    this.updateGraph()
+  }
+
+  updateGraph(){
+  this.dataSource = [];
   this.batchServ.getBatches().subscribe(
     (response) => {
       this.batches = response;
@@ -26,13 +35,19 @@ export class CalendarViewComponent implements OnInit {
         
         let sd = batch.startDate.split("-");
         let ed = batch.endDate.split("-");
+        // console.log(parseInt(sd[0]));
+        // console.log(this.years[this.pickedYear]);
+        if ((parseInt(sd[0]))==this.years[this.pickedYear]){
+          
         let d = {
           batchName: batch.batchName,
           trainer: batch.name,
+          
           start: new Date(parseInt(sd[0]), parseInt(sd[1]), parseInt(sd[2])),
           end: new Date(parseInt(ed[0]), parseInt(ed[1]), parseInt(ed[2]))
         };
         this.dataSource.push(d);
+      }
       }
     }
   )
@@ -40,6 +55,9 @@ export class CalendarViewComponent implements OnInit {
  
   }
 }
+
+
+
 
 class Data {
   batchName: string;
