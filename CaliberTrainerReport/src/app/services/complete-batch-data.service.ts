452082d3a228518450b0fd9665efbd1821bb6d6a
Jason Zelonka <jzelonka@ufl.edu>
2021-01-06 17:58:31 -0500

    getting all data, strange errors when implemented

--- CaliberTrainerReport/src/app/services/complete-batch-data.service.ts
+++ CaliberTrainerReport/src/app/services/complete-batch-data.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UrlService } from './url.service';
+import { Batch } from '../class/batch';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,9 @@ export class CompleteBatchDataService {
   constructor(private http: HttpClient, private urlServe: UrlService) { }
 
 
-  getCompleteBatchDataById(id: number) : Observable<Object>
+  getCompleteBatchDataById(id: String) : Observable<any>
   {
-    return this.http.get(this.urlServe.getUrl() + "/Complete/Batch" + id);
+    return this.http.get(this.urlServe.getUrl() + "/Complete/Batch/" + id);
   }
 
 }
