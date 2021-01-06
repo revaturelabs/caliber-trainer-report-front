ac4922315a1b7fb746a5bee7a63c237afa184262
Brian Conlon <brian.conlon18@myhunter.cuny.edu>
2021-01-06 17:03:32 -0500

    Changes for whitespace and initializing with one year

--- CaliberTrainerReport/src/app/Components/calendar-view/calendar-view.component.ts
+++ CaliberTrainerReport/src/app/Components/calendar-view/calendar-view.component.ts
@@ -12,7 +12,7 @@ export class CalendarViewComponent implements OnInit {
 
    batches:any[] =[];
   dataSource: Data[] = [];
-  years = [2015,2016,2017,2018,2019,2020];
+  years = [2015,2016,2017,2018,2019,2020,2021];
   year=0;
   yearObj=[];
   allYears=[];
@@ -21,6 +21,7 @@ export class CalendarViewComponent implements OnInit {
   constructor(private batchServ: GetBatchService) { }
 
   ngOnInit(){
+    this.years[this.pickedYear] = 2018;
     this.dataSource = [];
     this.batchServ.getBatches().subscribe(
       (response) => {
@@ -32,8 +33,8 @@ export class CalendarViewComponent implements OnInit {
           let ed = batch.endDate.split("-");
           // console.log(parseInt(sd[0]));
           // console.log(this.years[this.pickedYear]);
-          // if ((parseInt(sd[0]))==this.years[this.pickedYear]){
-            
+          
+         if ((parseInt(sd[0]))==this.years[this.pickedYear]){  
           let d = {
             batchName: batch.batchName,
             trainer: batch.name,
@@ -42,12 +43,23 @@ export class CalendarViewComponent implements OnInit {
             end: new Date(parseInt(ed[0]), parseInt(ed[1]), parseInt(ed[2]))
             
           };
+          let g = {
+            batchName: batch.batchName,
+            trainer: batch.name,
+  
+  
+            start: new Date(parseInt(sd[0]), 12, 31),
+            end: new Date(parseInt(sd[0]), 12, 31)
+          }
           console.log(d.end);
           this.dataSource.push(d);
+          this.dataSource.push(g);
         }
+      }
         }
       // }
     )
+    // this.years[this.pickedYear] = 2018;
     // this.updateGraph()
   }
 
@@ -64,7 +76,6 @@ export class CalendarViewComponent implements OnInit {
         // console.log(parseInt(sd[0]));
         // console.log(this.years[this.pickedYear]);
         if ((parseInt(sd[0]))==this.years[this.pickedYear]){
-          
         let d = {
           batchName: batch.batchName,
           trainer: batch.name,
@@ -72,7 +83,27 @@ export class CalendarViewComponent implements OnInit {
           start: new Date(parseInt(sd[0]), parseInt(sd[1]), parseInt(sd[2])),
           end: new Date(parseInt(ed[0]), parseInt(ed[1]), parseInt(ed[2]))
         };
+        // let y = {
+        //   batchName: batch.batchName,
+        //   trainer: batch.name,
+          
+
+        //   year : parseInt(sd[0]),
+        //   start: new Date(parseInt(sd[0]), 0, 1),
+        //   end: new Date (parseInt(sd[0]), 0, 1)
+        // }
+        let g = {
+          batchName: batch.batchName,
+          trainer: batch.name,
+
+
+          start: new Date(parseInt(sd[0]), 12, 31),
+          end: new Date(parseInt(sd[0]), 12, 31)
+        }
+        console.log(parseInt(sd[0]))
         this.dataSource.push(d);
+        this.dataSource.push(g);
+        // this.dataSource.push(y);
       }
       }
     }
--- CaliberTrainerReport/src/environments/environment.ts
+++ CaliberTrainerReport/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //backEndUrl: 'http://3.236.244.228:8081/excaliber/'
-  backEndUrl: 'http://localhost:8080/excaliber/'
+  backEndUrl: 'http://localhost:8081/excaliber/'
 };
 
 /*
