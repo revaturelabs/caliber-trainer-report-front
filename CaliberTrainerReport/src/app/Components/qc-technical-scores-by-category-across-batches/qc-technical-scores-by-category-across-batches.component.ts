9f0424777e36d18fccae601e4317a00a0d43440b
Christopher Cross <cc28@alumni.princeton.edu>
2021-01-06 12:09:15 -0800

    add check for empty batchByCategory array in qc-technical-scores-by-category-across-batches.component.ts

--- CaliberTrainerReport/src/app/Components/qc-technical-scores-by-category-across-batches/qc-technical-scores-by-category-across-batches.component.ts
+++ CaliberTrainerReport/src/app/Components/qc-technical-scores-by-category-across-batches/qc-technical-scores-by-category-across-batches.component.ts
@@ -9,7 +9,8 @@ import { FilterBatch } from 'src/app/utility/FilterBatch';
 
 @Component({
   selector: 'app-qc-technical-scores-by-category-across-batches',
-  templateUrl: './qc-technical-scores-by-category-across-batches.component.html',
+  templateUrl:
+    './qc-technical-scores-by-category-across-batches.component.html',
   styleUrls: ['./qc-technical-scores-by-category-across-batches.component.css'],
 })
 export class QcTechnicalScoresByCategoryAcrossBatchesComponent
@@ -36,19 +37,19 @@ export class QcTechnicalScoresByCategoryAcrossBatchesComponent
 
   batchNames: string[];
 
-  
   // this array tracks which batches to show on the graph
   // index of batchFlags corresponds to index of batchNames:string[]
   batchFlags: boolean[];
   // FilterBatch is a helper class located in utility folder under src > app
   // it contains a method called filterBatch(any[], boolean[]) that takes in any[] and returns a new any[] with true indices from boolean[]
   batchFilter: FilterBatch;
-  
+
   yValues: any[];
 
   // Dealing with Scalability
   width: number;
   isBig: boolean;
+  noScoresByCategoryData: boolean;
 
   constructor(
     private batchTechnicalStatusBySkillCategoryService: BatchTechnicalStatusBySkillCategoryService,
@@ -58,7 +59,6 @@ export class QcTechnicalScoresByCategoryAcrossBatchesComponent
 
   ngOnInit(): void {
     this.selectedValue = this.qcTS.selectedValue;
-    this.graphAdjust();
     this.categoriesObj = [];
     this.categoriesName = [];
     this.yValues = [];
@@ -77,38 +77,52 @@ export class QcTechnicalScoresByCategoryAcrossBatchesComponent
     this.batchFlags = [];
     this.batchFilter = new FilterBatch();
 
-
     this.BatchTechnicalStatusBySkillCategoryServiceSubscription = this.batchTechnicalStatusBySkillCategoryService
       .getAvgCategoryScoresObservables()
       .subscribe((resp) => {
         // Remove entries with no scores.
-        for(let i = resp.batchByCategory.length - 1; i >= 0; i--) {
+        for (let i = resp.batchByCategory.length - 1; i >= 0; i--) {
           let batchSum: number = 0;
-          for(const batch of resp.batchByCategory[i].batches) {
+          for (const batch of resp.batchByCategory[i].batches) {
             let scores = batch.score;
-            batchSum += scores.average + scores.good + scores.poor + scores.superstar;
+            batchSum +=
+              scores.average + scores.good + scores.poor + scores.superstar;
           }
-          if(batchSum == 0) {
+          if (batchSum == 0) {
             resp.batchByCategory.splice(i, 1);
           }
+        }
+
+        // In the event that all the entries are removed. We display
+        // a message to the user and do not display the data
+        // otherwise, we can adjust the graph
+        if (resp.batchByCategory.length === 0) {
+          console.log('no databyWeek to dislay');
+          this.noScoresByCategoryData = true;
+          return;
+        } else {
+          this.graphAdjust();
         }
 
         for (const obj of resp.batchByCategory) {
           this.categoriesName.push(obj.categoryName);
           this.categoriesObj.push(obj.batches);
         }
-        this.categoriesName.unshift("Overview");
+        this.categoriesName.unshift('Overview');
         this.categoriesObj.unshift(resp.batchByCategory[0].batches);
         // this.categoriesObj.unshift("COOL");
         this.setScoreValues();
-        
+
         for (const score of resp.batchByCategory[0].batches) {
           this.batchNames.push(score.batchName);
           this.batchFlags.push(true);
         }
 
         // These arguments might need to change.
-        this.displayGraph(this.batchFilter.filterBatch(this.batchNames,this.batchFlags), this.batchFilter.filterBatch(this.yValues,this.batchFlags));
+        this.displayGraph(
+          this.batchFilter.filterBatch(this.batchNames, this.batchFlags),
+          this.batchFilter.filterBatch(this.yValues, this.batchFlags)
+        );
       });
   }
 
@@ -124,103 +138,94 @@ export class QcTechnicalScoresByCategoryAcrossBatchesComponent
     this.cumulativeGood = [];
     this.cumulativePoor = [];
 
-    let trainerId = sessionStorage.getItem("selectedId");
-    let gA2: any[] = JSON.parse(sessionStorage.getItem("graphingArray2" + trainerId));
+    let trainerId = sessionStorage.getItem('selectedId');
+    let gA2: any[] = JSON.parse(
+      sessionStorage.getItem('graphingArray2' + trainerId)
+    );
 
     this.setScoreValues();
-    if(this.selectedValue ==0 ){
+    if (this.selectedValue == 0) {
       this.displayGraph(gA2[0], gA2[1]);
-      this.displayGraph(this.batchFilter.filterBatch(gA2[0],this.batchFlags), this.batchFilter.filterBatch(gA2[1], this.batchFlags));
+      this.displayGraph(
+        this.batchFilter.filterBatch(gA2[0], this.batchFlags),
+        this.batchFilter.filterBatch(gA2[1], this.batchFlags)
+      );
     } else {
-      this.displayGraph(this.batchFilter.filterBatch(this.batchNames,this.batchFlags), this.batchFilter.filterBatch(this.yValues, this.batchFlags));
+      this.displayGraph(
+        this.batchFilter.filterBatch(this.batchNames, this.batchFlags),
+        this.batchFilter.filterBatch(this.yValues, this.batchFlags)
+      );
     }
   }
 
   setScoreValues() {
-    if(this.pickedCategory == 0){
-      this.categoriesObj.forEach(c => {
-
+    if (this.pickedCategory == 0) {
+      this.categoriesObj.forEach((c) => {
         // Each object c is an individual category on the graph. We want to display ALL simultaneously.
-        
+
         // JAVA
         for (const stuff of c) {
+          const score = stuff.score;
+          let totalValue = 0;
+          let quantity = 0;
 
+          this.poorRawScore.push(score.poor);
+          this.averageRawScore.push(score.average);
+          this.goodRawScore.push(score.good);
+          this.superstarRawScore.push(score.superstar);
 
+          totalValue =
+            score.poor * 1 +
+            score.average * 2 +
+            score.good * 3 +
+            score.superstar * 4;
+          quantity = score.poor + score.average + score.good + score.superstar;
+
+          if (isNaN(totalValue / quantity)) {
+            this.yValues.push(0);
+          } else {
+            this.yValues.push(Math.round((totalValue / quantity) * 100) / 100);
+          }
+        }
+        this.cumulativePoor.push(this.poorRawScore);
+        this.poorRawScore = [];
+        this.cumulativeGood.push(this.goodRawScore);
+        this.goodRawScore = [];
+        this.cumulativeAverage.push(this.averageRawScore);
+        this.averageRawScore = [];
+        this.cumulativeSuper.push(this.superstarRawScore);
+        this.superstarRawScore = [];
+
+        this.multiGraphYValues.push(this.yValues);
+
+        this.yValues = [];
+      });
+    } else {
+      // For all the data we want to to view concerning a single week (category) chosen by user.
+      for (const stuff of this.categoriesObj[this.pickedCategory]) {
         const score = stuff.score;
         let totalValue = 0;
         let quantity = 0;
-  
+
         this.poorRawScore.push(score.poor);
         this.averageRawScore.push(score.average);
         this.goodRawScore.push(score.good);
         this.superstarRawScore.push(score.superstar);
-  
+
         totalValue =
           score.poor * 1 +
           score.average * 2 +
           score.good * 3 +
           score.superstar * 4;
-        quantity =
-          score.poor +
-          score.average +
-          score.good +
-          score.superstar;
-  
+        quantity = score.poor + score.average + score.good + score.superstar;
+
         if (isNaN(totalValue / quantity)) {
           this.yValues.push(0);
         } else {
-          this.yValues.push(
-            Math.round((totalValue / quantity) * 100) / 100
-          );
-        } 
-      }
-      this.cumulativePoor.push(this.poorRawScore);
-      this.poorRawScore = [];
-      this.cumulativeGood.push(this.goodRawScore);
-      this.goodRawScore = [];
-      this.cumulativeAverage.push(this.averageRawScore);
-      this.averageRawScore = [];
-      this.cumulativeSuper.push(this.superstarRawScore);
-      this.superstarRawScore = [];
-
-      this.multiGraphYValues.push(this.yValues);
-      
-      this.yValues = [];
-    
-    
-    });
-  } else {
-    // For all the data we want to to view concerning a single week (category) chosen by user.
-    for (const stuff of this.categoriesObj[this.pickedCategory]) {
-      const score = stuff.score;
-      let totalValue = 0;
-      let quantity = 0;
-
-      this.poorRawScore.push(score.poor);
-      this.averageRawScore.push(score.average);
-      this.goodRawScore.push(score.good);
-      this.superstarRawScore.push(score.superstar);
-
-      totalValue =
-        score.poor * 1 +
-        score.average * 2 +
-        score.good * 3 +
-        score.superstar * 4;
-      quantity =
-        score.poor +
-        score.average +
-        score.good +
-        score.superstar;
-
-      if (isNaN(totalValue / quantity)) {
-        this.yValues.push(0);
-      } else {
-        this.yValues.push(
-          Math.round((totalValue / quantity) * 100) / 100
-        );
+          this.yValues.push(Math.round((totalValue / quantity) * 100) / 100);
+        }
       }
     }
-  }
   }
 
   displayGraph(batchDisplayNames: string[], yDisplayValues: any[]) {
@@ -230,43 +235,55 @@ export class QcTechnicalScoresByCategoryAcrossBatchesComponent
 
     // Remove categories if they are not present in the shown batches.
     let batchRemoveEmptyReduce = (acc, curr, i) => {
-      if(this.batchFlags[i]) {
-        return acc += curr;
+      if (this.batchFlags[i]) {
+        return (acc += curr);
       } else {
         return acc;
       }
-    }
+    };
 
-    var colorArray = [ 
-      '#FF0000', '#FF8C00', 
-      '#FFD700',  '#228B22',
-      '#000080', '#4B0082',
-      '#FF00FF', '#800000',
-      '#00FF00', '#00CED1',
-      '#9370DB', '#000000',
-      '#6495ED', '#696969'
-    ]
+    var colorArray = [
+      '#FF0000',
+      '#FF8C00',
+      '#FFD700',
+      '#228B22',
+      '#000080',
+      '#4B0082',
+      '#FF00FF',
+      '#800000',
+      '#00FF00',
+      '#00CED1',
+      '#9370DB',
+      '#000000',
+      '#6495ED',
+      '#696969',
+    ];
 
     // An array of objects. Each object should contain a yDisplay array within.
     let lineData: any[] = [];
-    if(this.pickedCategory == 0){
+    if (this.pickedCategory == 0) {
       // Removes the first (redundant) element
       this.multiGraphYValues.shift();
-      for(let i = 1; i < this.categoriesName.length; i++){
-        if(this.multiGraphYValues[i-1].reduce(batchRemoveEmptyReduce, 0) !== 0) {
-          let lineColor:string;
-        
-          lineColor = colorArray[(i-1) % colorArray.length];
-  
+      for (let i = 1; i < this.categoriesName.length; i++) {
+        if (
+          this.multiGraphYValues[i - 1].reduce(batchRemoveEmptyReduce, 0) !== 0
+        ) {
+          let lineColor: string;
+
+          lineColor = colorArray[(i - 1) % colorArray.length];
+
           let dataObj = {
-            label: ''+this.categoriesName[i], // Name the series
-            data: this.batchFilter.filterBatch(this.multiGraphYValues[i-1],this.batchFlags), // Specify the data values array
+            label: '' + this.categoriesName[i], // Name the series
+            data: this.batchFilter.filterBatch(
+              this.multiGraphYValues[i - 1],
+              this.batchFlags
+            ), // Specify the data values array
             fill: false,
             borderColor: lineColor, // Add custom color border (Line)
             backgroundColor: '#000000', // Add custom color background (Points and Fill)
             borderWidth: 1, // Specify bar border width
           };
-  
+
           lineData.push(dataObj);
         }
       }
@@ -280,7 +297,7 @@ export class QcTechnicalScoresByCategoryAcrossBatchesComponent
         3: 'Good',
         4: 'Superstar',
       };
-  
+
       this.myLineChart = new Chart('secondChart', {
         type: 'line',
         data: {
@@ -312,73 +329,70 @@ export class QcTechnicalScoresByCategoryAcrossBatchesComponent
           },
         },
       });
-
-
     } else {
-      let lineColor:string = colorArray[(this.pickedCategory-1) % colorArray.length];
-    
+      let lineColor: string =
+        colorArray[(this.pickedCategory - 1) % colorArray.length];
 
-    const yLabels = {
-      0: 'No Data',
-      1: 'Poor',
-      2: 'Average',
-      3: 'Good',
-      4: 'Superstar',
-    };
+      const yLabels = {
+        0: 'No Data',
+        1: 'Poor',
+        2: 'Average',
+        3: 'Good',
+        4: 'Superstar',
+      };
 
-    this.myLineChart = new Chart('secondChart', {
-      type: 'line',
-      data: {
-        labels: batchDisplayNames,
-        datasets: [
-          {
-            label: 'Overall Average', // Name the series
-            data: yDisplayValues, // Specify the data values array
-            fill: false,
-            borderColor: lineColor, // Add custom color border (Line)
-            backgroundColor: '#000000', // Add custom color background (Points and Fill)
-            borderWidth: 1, // Specify bar border width
-          },
-        ],
-      },
-      options: {
-        scales: {
-          yAxes: [
+      this.myLineChart = new Chart('secondChart', {
+        type: 'line',
+        data: {
+          labels: batchDisplayNames,
+          datasets: [
             {
-              ticks: {
-                beginAtZero: true,
-                suggestedMax: 4,
-                stepSize: 1,
-                callback(value, index, values) {
-                  return yLabels[value];
-                },
-              },
+              label: 'Overall Average', // Name the series
+              data: yDisplayValues, // Specify the data values array
+              fill: false,
+              borderColor: lineColor, // Add custom color border (Line)
+              backgroundColor: '#000000', // Add custom color background (Points and Fill)
+              borderWidth: 1, // Specify bar border width
             },
           ],
         },
-        title: {
-          display: true,
-          text: `QC scores based on ${
-            this.categoriesName[this.pickedCategory]
-          }`,
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                  suggestedMax: 4,
+                  stepSize: 1,
+                  callback(value, index, values) {
+                    return yLabels[value];
+                  },
+                },
+              },
+            ],
+          },
+          title: {
+            display: true,
+            text: `QC scores based on ${
+              this.categoriesName[this.pickedCategory]
+            }`,
+          },
+          hover: {
+            mode: 'nearest',
+            intersect: true,
+          },
         },
-        hover: {
-          mode: 'nearest',
-          intersect: true,
-        },
-      },
-    });
-  }
+      });
+    }
   }
 
   graphAdjust() {
     const chartElem = document.getElementById('divChart2');
     this.isBig = this.displayGraphService.graphAdjust(
-          chartElem,
-          this.qcTS.selectedValue,
-          this.isBig
-        );
-
+      chartElem,
+      this.qcTS.selectedValue,
+      this.isBig
+    );
   }
 
   @HostListener('window:resize', ['$event'])
@@ -399,19 +413,20 @@ export class QcTechnicalScoresByCategoryAcrossBatchesComponent
   }
 
   ngOnDestroy() {
-    if(this.BatchTechnicalStatusBySkillCategoryServiceSubscription != undefined){
-
+    if (
+      this.BatchTechnicalStatusBySkillCategoryServiceSubscription != undefined
+    ) {
       this.BatchTechnicalStatusBySkillCategoryServiceSubscription.unsubscribe();
     }
   }
 
-  toggle(index: number): void{
+  toggle(index: number): void {
     this.batchFlags[index] = !this.batchFlags[index];
     this.updateGraph();
   }
 
   batch_dropdown_flag: boolean = true;
-  toggleBatchDropdown(): void{
+  toggleBatchDropdown(): void {
     this.batch_dropdown_flag = !this.batch_dropdown_flag;
   }
 }
