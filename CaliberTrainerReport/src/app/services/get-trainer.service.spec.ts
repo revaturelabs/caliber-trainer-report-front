2d2a71cdacf34e68a7e673433d9686f0770c3b4c
Do Yeun Kim <doyeunkim.vm@gmail.com>
2021-01-12 10:45:30 -0500

    other than the try-catch block which I can't test, 100% coverage

--- CaliberTrainerReport/src/app/services/get-trainer.service.spec.ts
+++ CaliberTrainerReport/src/app/services/get-trainer.service.spec.ts
@@ -1,19 +1,36 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { UrlService } from './url.service';
 import { TestBed } from '@angular/core/testing';
+import { environment } from '../../environments/environment'
 
 import { GetTrainerService } from './get-trainer.service';
 
-describe('GetTrainerService', () => {
+fdescribe('GetTrainerService', () => {
   let service: GetTrainerService;
+  let urlServ: UrlService;
 
-  beforeEach(() => {
+  beforeEach(async() => {
     TestBed.configureTestingModule({
-      imports:[HttpClientTestingModule]
+      imports:[HttpClientModule],
+      providers:[GetTrainerService, UrlService]
     });
     service = TestBed.inject(GetTrainerService);
+    urlServ = TestBed.inject(UrlService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should set the currentTrainers item for session storage upon successful getTrainerList()', async() => {
+    console.log(await service.getTrainerList());
+    expect(sessionStorage.getItem('currentTrainers')).toEqual('[]');
+  });
+
+  it('should get all the trainers', async() => {
+    const trainers = await service.getAllTrainers().toPromise();
+    expect(trainers).toBeTruthy();
+    expect(trainers.length).toBeGreaterThan(0);
+  });
+
 });
