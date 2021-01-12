c9e3fd723e2a957f5c6ab588bdb2cfc07ffc78b9
irtalla <45830357+irtalla@users.noreply.github.com>
2021-01-12 18:15:21 -0500

    Update qc-technical-scores-by-category-across-batches.component.ts
    Chose FilterPipe with proper path

--- CaliberTrainerReport/src/app/Components/qc-technical-scores-by-category-across-batches/qc-technical-scores-by-category-across-batches.component.ts
+++ CaliberTrainerReport/src/app/Components/qc-technical-scores-by-category-across-batches/qc-technical-scores-by-category-across-batches.component.ts
@@ -6,12 +6,7 @@ import { QCComponent } from 'src/app/Components/qc/qc.component';
 import { Subscription } from 'rxjs';
 import { DisplayGraphService } from 'src/app/services/display-graph.service';
 import { FilterBatch } from 'src/app/utility/FilterBatch';
-<<<<<<< HEAD
 import { FilterPipe } from 'src/app/filter.pipe';
-
-=======
-import {FilterPipe} from '../../filter.pipe';
->>>>>>> 2531810051ab261ffa80156e8c43f12ea5993a22
 
 @Component({
   selector: 'app-qc-technical-scores-by-category-across-batches',
