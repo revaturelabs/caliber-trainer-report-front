7881f6f1d91a72195ce99de2498a048cadfdb730
Christopher Cross <cc28@alumni.princeton.edu>
2021-01-12 11:17:25 -0800

    standardize and enforce error handling in frontend services

--- CaliberTrainerReport/src/app/services/complete-batch-data.service.ts
+++ CaliberTrainerReport/src/app/services/complete-batch-data.service.ts
@@ -3,18 +3,23 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UrlService } from './url.service';
 import { Batch } from '../class/batch';
+import { ErrorHandlerService } from './error-handler.service';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CompleteBatchDataService {
+  constructor(
+    private http: HttpClient,
+    private urlServe: UrlService,
+    private errorHandler: ErrorHandlerService
+  ) {}
 
-  constructor(private http: HttpClient, private urlServe: UrlService) { }
-
-
-  getCompleteBatchDataById(id: String) : Observable<any>
-  {
-    return this.http.get(this.urlServe.getUrl() + "/Complete/Batch/" + id);
+  getCompleteBatchDataById(id: String): Observable<any> {
+    return this.http.get(this.urlServe.getUrl() + '/Complete/Batch/' + id).pipe(
+      map((resp) => resp),
+      catchError(this.errorHandler.handleError)
+    );
   }
-
 }
--- CaliberTrainerReport/src/app/services/get-batch.service.ts
+++ CaliberTrainerReport/src/app/services/get-batch.service.ts
@@ -1,32 +1,50 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { UrlService } from 'src/app/services/url.service';
 import { Batch } from '../class/batch';
-
+import { ErrorHandlerService } from './error-handler.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GetBatchService {
+  constructor(
+    private url: UrlService,
+    private http: HttpClient,
+    private errorHandler: ErrorHandlerService
+  ) {}
 
-  constructor(private url:UrlService, private http: HttpClient) { }
-
-  getBatches():Observable<any> {
+  getBatches(): Observable<any> {
     let id: string = sessionStorage.getItem('selectedId');
-    return this.http.get(this.url.getUrl() + "/Trainer/batches/" + id);
+    return this.http.get(this.url.getUrl() + '/Trainer/batches/' + id).pipe(
+      map((resp) => resp as any),
+      catchError(this.errorHandler.handleError)
+    );
   }
 
-  getBatchesByTrainerEmail(email : string) : Observable<string[]> {
-    return this.http.post<string[]>(this.url.getUrl() + "/Batch/batches", email);
+  getBatchesByTrainerEmail(email: string): Observable<string[]> {
+    return this.http
+      .post<string[]>(this.url.getUrl() + '/Batch/batches', email)
+      .pipe(
+        map((resp) => resp as string[]),
+        catchError(this.errorHandler.handleError)
+      );
   }
 
-  getBatchById(batchId : string) : Observable<Batch>{
-    return this.http.get<Batch>(this.url.getUrl() + "/Batch/batch/"+batchId);
+  getBatchById(batchId: string): Observable<Batch> {
+    return this.http
+      .get<Batch>(this.url.getUrl() + '/Batch/batch/' + batchId)
+      .pipe(
+        map((resp) => resp as Batch),
+        catchError(this.errorHandler.handleError)
+      );
   }
 
-  getPromiseBatchById(batchId : string) : Promise<Batch>{
-    return this.http.get<Batch>(this.url.getUrl() + "/Batch/batch/"+batchId).toPromise();
+  getPromiseBatchById(batchId: string): Promise<Batch> {
+    return this.http
+      .get<Batch>(this.url.getUrl() + '/Batch/batch/' + batchId)
+      .toPromise();
   }
-
 }
