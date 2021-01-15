296bffa2c5a049985b951338b81e58c98a925a08
Christopher Cross <cc28@alumni.princeton.edu>
2021-01-15 09:22:28 -0800

    re-inject errorHanlder dependency

--- CaliberTrainerReport/src/app/services/TechnicalStatusByWeek.service.ts
+++ CaliberTrainerReport/src/app/services/TechnicalStatusByWeek.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { UrlService } from './url.service';
 import { Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
-// import { ErrorHandlingServiceService } from './error-handling-service.service';
+import { ErrorHandlerService } from './error-handler.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,16 +12,15 @@ export class TechnicalStatusByWeekService {
   constructor(
     private http: HttpClient,
     private urlService: UrlService,
-    // private errorHandler: ErrorHandlingServiceService
+    private errorHandler: ErrorHandlerService
   ) {}
 
   getTechnicalStatusByWeek(): Observable<any> {
     return this.http
       .get(this.urlService.getUrlWithId() + 'TechnicalStatusByWeek')
       .pipe(
-        map((resp) => resp)
-        // map((resp) => resp),
-        // catchError(this.errorHandler.handleError)
+        map((resp) => resp),
+        catchError(this.errorHandler.handleError)
       );
   }
 }
