8d91e0bb025340774317163ad30bc2ed02f36e56
Christopher Cross <cc28@alumni.princeton.edu>
2021-01-15 09:10:53 -0800

    readd errorHandler service

---
+++ CaliberTrainerReport/src/app/services/error-handler.service.ts
@@ -0,0 +1,25 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ErrorHandlerService {
+  constructor() {}
+
+  public handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError('Something bad happened; please try again later.');
+  }
+}
