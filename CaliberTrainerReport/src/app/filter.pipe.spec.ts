562ccfbd54a23d010bc998255c45f2a1a6807a6a
Do Yeun Kim <doyeunkim.vm@gmail.com>
2021-01-15 15:14:28 -0500

    filter.pipe.spec.ts written

--- CaliberTrainerReport/src/app/filter.pipe.spec.ts
+++ CaliberTrainerReport/src/app/filter.pipe.spec.ts
@@ -1,12 +1,46 @@
 import { FilterPipe } from './filter.pipe';
 
-describe('FilterPipe', () => {
+fdescribe('FilterPipe', () => {
+
   it('create an instance', () => {
     const pipe = new FilterPipe();
     expect(pipe).toBeTruthy();
   });
 
-  it('will return an empty ', () => {
+  it('will return an empty array because of invalid input', () => {
+    const pipe = new FilterPipe();
+    const validArray = ['hello'];
+    const invalidArray = [];
+    let validString:string = 'hello';
+    let emptyString:string = '';
+    let nullString:string;
 
+    expect(pipe.transform(validArray, emptyString)).toEqual([]);
+    expect(pipe.transform(validArray, nullString)).toEqual([]);
+    expect(pipe.transform(invalidArray, validString)).toEqual([]);
+    expect(pipe.transform(invalidArray, emptyString)).toEqual([]);
+    expect(pipe.transform(invalidArray, nullString)).toEqual([]);
+  });
+
+  it('will return an empty array from successful filtering', () => {
+    const pipe = new FilterPipe();
+    const validArray = ['hello'];
+    let invalidString:string = 'world';
+    
+    expect(pipe.transform(validArray, invalidString)).toEqual([]);
+  });
+
+  it('will return a filled array from successful filtering', () => {
+    const pipe = new FilterPipe();
+    const array = ['hello', 'world', 'Something', 'else'];
+    let helloStr:string = 'hello';
+    let worldStr:string = 'world';
+    let somethingStr:string = 'something';
+    let elseStr:string = 'ELSE';
+
+    expect(pipe.transform(array, helloStr)).toEqual(['hello']);
+    expect(pipe.transform(array, worldStr)).toEqual(['world']);
+    expect(pipe.transform(array, somethingStr)).toEqual(['Something']);
+    expect(pipe.transform(array, elseStr)).toEqual(['else']);
   });
 });
