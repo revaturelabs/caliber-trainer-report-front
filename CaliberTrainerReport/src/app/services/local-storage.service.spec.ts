6f311c21bd80a39ba34f7923761f108e6b9da0ef
Ian Talla <ianroi_talla@yahoo.com>
2021-01-19 18:48:32 -0500

    Added tests for local-storage.service

--- CaliberTrainerReport/src/app/services/local-storage.service.spec.ts
+++ CaliberTrainerReport/src/app/services/local-storage.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { LocalStorageService } from './local-storage.service';
 
-fdescribe('LocalStorageService', () => {
+describe('LocalStorageService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
