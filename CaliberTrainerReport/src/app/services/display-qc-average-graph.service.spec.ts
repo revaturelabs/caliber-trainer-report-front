755579a2b4f31d31a13a046616a5bf1afa7b67d1
Ian Talla <ianroi_talla@yahoo.com>
2021-01-19 14:28:21 -0500

    Did all I could for display-qc-average-graph.service.ts

--- CaliberTrainerReport/src/app/services/display-qc-average-graph.service.spec.ts
+++ CaliberTrainerReport/src/app/services/display-qc-average-graph.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { DisplayQcAverageGraphService } from './display-qc-average-graph.service';
 
-describe('DisplayQcAverageGraphService', () => {
+fdescribe('DisplayQcAverageGraphService', () => {
   let service: DisplayQcAverageGraphService;
 
   beforeEach(() => {
@@ -12,5 +12,49 @@ describe('DisplayQcAverageGraphService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  //the resizing thing cannot be done in Jasmine/Karma so it is useless.
+
+  it('should make a graph with name "Homestuck" when I give it the title "Homestuck"', () => {
+    let newGraphOptions = service.graphOptions("Homestuck");
+    expect(newGraphOptions.title.text).toBe("Homestuck");
+    //because I literally can't test any other aspects of the code.
+
+    let fakeData = {data: {
+      datasets: [
+        {label: "John Egbert"},
+        {label: "Rose Lalonde"},
+        {label: "Dave Strider"},
+        {label: "Jade Harley"}
+      ]
+    }};
+
+    let fakePoorTooltipInfo = {
+      yValue: 1,
+      datasetIndex: 0,
+      value: "1"
+    };
+    let fakeAverageTooltipInfo = {
+      yValue: 1,
+      datasetIndex: 0,
+      value: "2"
+    };
+    let fakeGoodTooltipInfo = {
+      yValue: 1,
+      datasetIndex: 0,
+      value: "3"
+    };
+    let fakeSuperstarTooltipInfo = {
+      yValue: 1,
+      datasetIndex: 0,
+      value: "4"
+    };
+
+    let poorResult = newGraphOptions.tooltips.callbacks.label(fakePoorTooltipInfo, fakeData);
+    expect(poorResult).toBe("John Egbert: Poor");
+
+    let averageResult = newGraphOptions.tooltips.callbacks.label(fakeAverageTooltipInfo, fakeData);
+    expect(averageResult).toBe("John Egbert: Average");
   });
 });
