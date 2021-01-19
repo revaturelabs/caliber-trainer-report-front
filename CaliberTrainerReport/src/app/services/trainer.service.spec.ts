f9bb1c2234946d21cec6ed77558319acd2ace51f
Do Yeun Kim <doyeunkim.vm@gmail.com>
2021-01-18 22:47:06 -0500

    increased test coverage for url service and working on trainer service

--- CaliberTrainerReport/src/app/services/trainer.service.spec.ts
+++ CaliberTrainerReport/src/app/services/trainer.service.spec.ts
@@ -1,19 +1,54 @@
+import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Trainer } from '../class/trainer';
 
 import { TrainerService } from './trainer.service';
 
-describe('TrainerService', () => {
+fdescribe('TrainerService', () => {
   let service: TrainerService;
 
-  beforeEach(() => {
+  beforeEach(async() => {
     TestBed.configureTestingModule({
-      imports:[HttpClientTestingModule]
+      imports:[HttpClientTestingModule, HttpClientModule]
     });
     service = TestBed.inject(TrainerService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should set currentTrainers after getting trainer list', async() => {
+    sessionStorage.setItem('currentTrainers', undefined);
+    await service.getTrainerList();
+    expect(sessionStorage.getItem('currentTrainers')).toBeTruthy();
+  });
+
+  it('should get all trainers', () => {
+    expect(service.getAllTrainers()).toBeTruthy();
+  });
+
+  it('should get a list of trainers', () => {
+    expect(service.setTrainerList([])).toBeTruthy();
+  });
+
+  it('should get selected value of some sort', () => {
+    let trainer:Trainer[] = [{
+      id: 1,
+      firstName: 'name',
+      lastName: 'name',
+      email: 'email'
+    }];
+    sessionStorage.setItem('currentTrainers', JSON.stringify(trainer));
+    service.getSelectedValue([]);
+    expect(sessionStorage.getItem('selectedId')).toEqual('1');
+    
+    sessionStorage.clear();
+
+    let fakeTrainer:Trainer[] = [];
+    sessionStorage.setItem('currentTrainers', JSON.stringify(fakeTrainer));
+    service.getSelectedValue([]);
+    expect(sessionStorage.getItem('selectedId')).toEqual(null);
   });
 });
